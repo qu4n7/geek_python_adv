196967d3683b8f6760f8b29d0d34f04e1fa44ec2
qu4n7 <13035@protonmail.com>
2019-07-30 12:51:27 +0300

    задания урока 5. тесты

---
+++ server/tests/test_validate_request.py
@@ -0,0 +1,40 @@
+import pytest 
+
+from datetime import datetime
+
+from protocol import validate_request
+
+@pytest.fixture 
+def expected_action():
+    return 'test'
+
+@pytest.fixture 
+def expected_code():
+    return 200
+
+@pytest.fixture 
+def expected_data():
+    return 'some data'
+
+@pytest.fixture
+def valid_request(expected_action, expected_data):
+    return {
+        'action': expected_action,
+        'time': datetime.now().timestamp(),
+        'data': expected_data,
+    }
+
+@pytest.fixture
+def invalid_request():
+    return {
+        'invalid_key': 'invalid_value'
+    }
+
+def test_valid_validate_request(valid_request):
+    is_valid = validate_request(valid_request)
+    assert is_valid
+
+def test_invalid_validate_request(invalid_request):
+    is_valid = validate_request(invalid_request)
+    assert not is_valid
+
